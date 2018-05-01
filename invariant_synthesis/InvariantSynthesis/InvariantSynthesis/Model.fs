2ccc314f171b437bf2c0009806a50ced1a87dfe0
E-Sh4rk <esh4rk@gmail.com>
2018-05-01 16:19:56 -0400

    f# base

---
+++ invariant_synthesis/InvariantSynthesis/InvariantSynthesis/Model.fs
@@ -0,0 +1,20 @@
+﻿namespace Model
+
+(* TYPES TO DESCRIBE (FINITE) MODELS *)
+
+type value = string * int // Type, uninterpreted runtime value
+
+type StructualOperator = Eq | Diff //| SSmaller // '<' can be considered as a regular relation
+type StructuralConstraint = value * StructualOperator * value
+type RelationConstraint = string * List<value> * bool
+type FunctionConstraint = string * List<value> * value
+type VariableConstraint = string * value
+
+type Constraint =
+    | Structural of StructuralConstraint
+    | Relation of RelationConstraint
+    | Function of FunctionConstraint
+    | Variable of VariableConstraint
+
+type Model = { s : StructuralConstraint; r : RelationConstraint; f : FunctionConstraint; v : VariableConstraint }
+
---
+++ invariant_synthesis/InvariantSynthesis/InvariantSynthesis/Program.fs
@@ -0,0 +1,8 @@
+﻿
+open System
+open Microsoft.Z3;
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    0
