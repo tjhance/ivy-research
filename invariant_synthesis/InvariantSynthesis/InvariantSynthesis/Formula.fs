c0ae3f21ff2b93fe41f86cedafcbdb3edfd24298
E-Sh4rk <esh4rk@gmail.com>
2018-05-15 21:19:48 -0400

    update

--- invariant_synthesis/InvariantSynthesis/InvariantSynthesis/Formula.fs
+++ invariant_synthesis/InvariantSynthesis/InvariantSynthesis/Formula.fs
@@ -11,6 +11,12 @@
         | ConstBool _ -> Bool
         | ConstInt (s,_) -> Uninterpreted s
 
+    type ValueAssociation =
+        | VAConst of ConstValue
+        | New of string
+        | ExistingVar of string
+        | ExistingFun of string * List<ConstValue>
+
     let formula_from_marks (env:Model.Environment) (m:Synthesis.Marks) (ad:Synthesis.AdditionalData) =
         // Associate a var to each value
         let next_name_nb = ref 0
@@ -18,26 +24,32 @@
             let c = (char)(65 + !next_name_nb)
             next_name_nb := !next_name_nb + 1
             c.ToString()
-
+            
         let vars_map = ref Map.empty
 
-        // Associates an existing var to a value if the constraint allows it
+        // Associates an existing variable to a value
         let associate_existing_var str =
             let cv = Map.find str env.v
-            vars_map := Map.add cv (str, false) !vars_map
+            if not (Map.containsKey cv !vars_map) then
+                vars_map := Map.add cv (ExistingVar str) !vars_map
+
+        // Associates an existing function to a value
+        let associate_existing_fun (str,cvs) =
+            let cv = Map.find (str,cvs) env.f
+            if not (Map.containsKey cv !vars_map) then
+                vars_map := Map.add cv (ExistingFun (str, cvs)) !vars_map
 
         // Return the associated var or CREATES a new existentially quantified var
         let value2var cv =
             match cv with
-            | cv when not (Synthesis.is_model_dependent_value cv) -> ValueConst cv
+            | cv when not (Synthesis.is_model_dependent_value cv) -> VAConst cv
             | cv ->
                 try
-                    let (str, _) = Map.find cv !vars_map
-                    ValueVar (str)
+                    Map.find cv !vars_map
                 with :? System.Collections.Generic.KeyNotFoundException ->
                     let name = new_var_name ()
-                    vars_map := Map.add cv (name, true) !vars_map
-                    ValueVar name
+                    vars_map := Map.add cv (New name) !vars_map
+                    New name
 
         let value_assigned cv =
             match cv with
@@ -46,8 +58,17 @@
 
         let all_new_vars_decl_assigned () : List<VarDecl> =
             let content = (Map.toList !vars_map)
-            let content = List.filter (fun (_,(_,b)) -> b) content
-            List.map (fun (cv,(v,_)) -> { Name=v ; Type=type_of_const_value cv }) content
+            let content = List.filter (fun (_,assoc) -> match assoc with New _ -> true | _ -> false) content
+            List.map (fun (cv,assoc) -> match assoc with New str -> { Name=str ; Type=type_of_const_value cv } | _ -> failwith "Invalid association.") content
+
+        let rec value_of_association va =
+            match va with
+            | VAConst cv -> ValueConst cv
+            | New str -> ValueVar str
+            | ExistingVar str -> ValueVar str
+            | ExistingFun (str, cvs) ->
+                let vs = List.map (fun cv -> value_of_association (value2var cv)) cvs
+                ValueFun (str, vs)
 
         // Browse the constraints to associate an existing var to values when possible
         Set.iter (associate_existing_var) m.v
@@ -56,9 +77,20 @@
                 (
                     fun str ->
                         let cv = Map.find str env.v
-                        value2var cv <> ValueVar str
+                        value2var cv <> ExistingVar str
                 ) m.v
         let m = {m with v=v'}
+
+        // Browse the constraints to associate an existing fun to values when possible
+        Set.iter (associate_existing_fun) m.f
+        let f' = // We remove trivial equalities
+            Set.filter
+                (
+                    fun (str, cvs) ->
+                        let cv = Map.find (str, cvs) env.f
+                        value2var cv <> ExistingFun (str, cvs)
+                ) m.f
+        let m = {m with f=f'}
 
         // Replace value by var in each var/fun marked constraint
         let constraints_var =
@@ -66,15 +98,15 @@
                 (
                     fun str ->
                         let cv = Map.find str env.v
-                        Equal (ValueVar str, value2var cv)
+                        Equal (ValueVar str, value_of_association (value2var cv))
                 ) m.v
         let constraints_fun =
             Set.map
                 (
                     fun (str,cvs) ->
                         let cv = Map.find (str,cvs) env.f
-                        let cvs = List.map value2var cvs
-                        Equal (ValueFun (str, cvs), value2var cv)
+                        let cvs = List.map (fun cv -> value_of_association (value2var cv)) cvs
+                        Equal (ValueFun (str, cvs), value_of_association (value2var cv))
                 ) m.f
         let constraints = Set.union constraints_var constraints_fun
 
@@ -86,7 +118,7 @@
                 (
                     fun (cv1,cv2) ->
                         let (cv1,cv2) = order_tuple (cv1,cv2)
-                        Not (Equal (value2var cv1, value2var cv2))
+                        Not (Equal (value_of_association (value2var cv1), value_of_association (value2var cv2)))
                 ) ineq_constraints
         let constraints = Set.union constraints ineq_constraints
 
