6f2620b6ec528f659c48ef69060851ec34d450ac
E-Sh4rk <esh4rk@gmail.com>
2018-05-19 20:33:16 -0400

    update

---
+++ invariant_synthesis/InvariantSynthesis/InvariantSynthesis/TInterpreter.fs
@@ -0,0 +1,13 @@
+﻿module TInterpreter
+
+    open AST
+    open Trace
+
+    exception ExprAssertionFailed of TrExpression * Formula
+    exception StAssertionFailed of TrStatement * Formula
+
+    let rec trace_expression (m:ModuleDecl) infos (env:Model.Environment) e =
+        match e with
+        | ExprConst cv ->
+            let red = (env,env,Some cv)
+            TrExprConst (red, cv)
