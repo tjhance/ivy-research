b36587f68b7a1f5827f4f9e70754b57c8d96d9d2
E-Sh4rk <esh4rk@gmail.com>
2018-05-29 18:22:33 -0400

    new trace system

--- invariant_synthesis/InvariantSynthesis/InvariantSynthesis/TInterpreter.fs
+++ invariant_synthesis/InvariantSynthesis/InvariantSynthesis/TInterpreter.fs
@@ -11,3 +11,30 @@
         | ExprConst cv ->
             let red = (env,env,Some cv)
             TrExprConst (red, cv)
+        | ExprVar v ->
+            let red = (env,env,Some (Interpreter.evaluate_value infos env (ValueVar v)))
+            TrExprVar (red, v)
+        | ExprFun (str, lst) ->
+            let lst = trace_expressions m infos env lst
+            let last_env = final_env_of_exprs lst env
+            let retval =
+                if exprs_are_fully_evaluated lst
+                then
+                    let rets = List.map ret_value_of_expr lst
+                    let rets = List.map (fun cv -> ValueConst cv) rets
+                    Some (Interpreter.evaluate_value infos last_env (ValueFun (str, rets)))
+                else None
+            let red = (env, last_env, retval)
+            TrExprFun (red, str, lst)
+
+    and trace_expressions (m:ModuleDecl) infos (env:Model.Environment) es =
+        let aux trs e =
+            match trs with
+            | [] -> [trace_expression m infos env e]
+            | hd::_ ->
+                if expr_is_fully_evaluated hd
+                then
+                    let env = final_env_of_expr hd
+                    (trace_expression m infos env e)::trs
+                else (TrExprNotEvaluated)::trs
+        List.rev (List.fold aux [] es)
--- invariant_synthesis/InvariantSynthesis/InvariantSynthesis/Trace.fs
+++ invariant_synthesis/InvariantSynthesis/InvariantSynthesis/Trace.fs
@@ -2,6 +2,7 @@
 
     open AST
     open Model
+    open System.Drawing
 
     type RuntimeStData = Environment * Environment
     type RuntimeExprData = Environment * Environment * ConstValue option
@@ -9,7 +10,7 @@
     type TrExpression =
         | TrExprConst of RuntimeExprData * ConstValue
         | TrExprVar of RuntimeExprData * string
-        | TrExprFun of RuntimeExprData * List<TrExpression>
+        | TrExprFun of RuntimeExprData * string * List<TrExpression>
         | TrExprAction of RuntimeExprData * List<VarDecl> * VarDecl * List<TrExpression> * TrStatement
         | TrExprAbstract of RuntimeExprData
         | TrExprEqual of RuntimeExprData * TrExpression * TrExpression
@@ -33,3 +34,52 @@
         | TrIfSomeElse of RuntimeStData * Option<ConstValue> * VarDecl * Formula * TrStatement
         | TrAssert of RuntimeStData * Formula
         | TrNotEvaluated
+
+    let runtime_data_of_expr expr =
+        match expr with
+        | TrExprConst (red,_)
+        | TrExprVar (red,_)
+        | TrExprFun (red,_,_)
+        | TrExprAction (red,_,_,_,_)
+        | TrExprAbstract red
+        | TrExprEqual (red,_,_)
+        | TrExprOr (red,_,_)
+        | TrExprAnd (red,_,_)
+        | TrExprNot (red,_)
+        | TrExprSomeElse (red,_,_,_) -> Some red
+        | TrExprNotEvaluated -> None
+
+    let expr_is_fully_evaluated expr =
+        match runtime_data_of_expr expr with
+        | None -> false
+        | Some (_,_,None) -> false
+        | Some (_,_,Some _) -> true
+
+    let exprs_are_fully_evaluated exprs =
+        List.forall expr_is_fully_evaluated exprs
+
+    exception NoRuntimeData
+    exception NotFullyEvaluated
+
+    let final_env_of_expr expr =
+        match runtime_data_of_expr expr with
+        | None -> raise NoRuntimeData
+        | Some (_,env,_) -> env
+
+    let final_env_of_exprs exprs initial_env =
+        let aux acc e =
+            try
+                final_env_of_expr e
+            with :? NoRuntimeData -> acc
+        List.fold aux initial_env exprs
+
+    let initial_env_of_expr expr =
+        match runtime_data_of_expr expr with
+        | None -> raise NoRuntimeData
+        | Some (env,_,_) -> env
+
+    let ret_value_of_expr expr =
+        match runtime_data_of_expr expr with
+        | None -> raise NoRuntimeData
+        | Some (_,_,None) -> raise NotFullyEvaluated
+        | Some (_,_,Some ret) -> ret
