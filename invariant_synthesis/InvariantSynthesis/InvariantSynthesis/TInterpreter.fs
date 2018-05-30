150bcb869d1525cd5edb04a8a8c9847646d62ecb
E-Sh4rk <esh4rk@gmail.com>
2018-05-30 17:24:56 -0400

    update

--- invariant_synthesis/InvariantSynthesis/InvariantSynthesis/TInterpreter.fs
+++ invariant_synthesis/InvariantSynthesis/InvariantSynthesis/TInterpreter.fs
@@ -44,9 +44,7 @@
             let (red, trs) = apply_op lst eval
             TrExprFun (red, str, trs)
         | ExprAction (str, lst) ->
-            let lst = trace_expressions m infos env lst
-            let last_env = final_env_of_exprs lst env
-            trace_action m infos last_env str lst
+            trace_action m infos env str lst
         | ExprEqual (e1, e2) ->
             let eval cv1 cv2 =
                 ConstBool (Interpreter.value_equal infos cv1 cv2)
@@ -73,6 +71,60 @@
                 else (TrExprNotEvaluated)::trs
         List.rev (List.fold aux [] es)
 
+    and trace_statement (m:ModuleDecl) infos (env:Model.Environment) s =
+        match s with
+        | NewBlock (decls, ss) ->
+            let env' = Interpreter.enter_new_block infos env decls (List.map (fun _ -> None) decls)
+            let trs = trace_statements m infos env' ss
+            let env' = final_env_of_sts trs env'
+            let env' = Interpreter.leave_block infos env' decls env
+            let rsd = (env, env', sts_are_fully_executed trs)
+            TrNewBlock (rsd, decls, trs)
+        | Expression e ->
+            let tr = trace_expression m infos env e
+            let env' = final_env_of_exprs [tr] env
+            let rsd = (env, env', expr_is_fully_evaluated tr)
+            TrExpression (rsd, tr)
 
-    and trace_action (m:ModuleDecl) infos (env:Model.Environment) name trargs =
-        TrExprNotEvaluated// TODO
+    and trace_statements (m:ModuleDecl) infos (env:Model.Environment) ss =
+        let rec aux env ss =
+            match ss with
+            | [] -> []
+            | s::ss ->
+                let tr = trace_statement m infos env s
+                if st_is_fully_executed tr
+                then
+                    let env = final_env_of_st tr
+                    tr::(aux env ss)
+                else [tr]
+        aux env ss
+
+    and trace_inline_action (m:ModuleDecl) infos (env:Model.Environment) input output (effect:Model.Environment->TrStatement) args =
+        let tr_args = trace_expressions m infos env args
+        let env' = final_env_of_exprs tr_args env
+        if exprs_are_fully_evaluated tr_args
+        then
+            let args = List.map ret_value_of_expr tr_args
+            let env'' = Interpreter.enter_new_block infos env' (output::input) (None::(List.map (fun a -> Some a) args))
+            let tr = effect env''
+            let env'' = final_env_of_sts [tr] env''
+
+            let res =
+                if st_is_fully_executed tr
+                then
+                    match Map.tryFind output.Name env''.v with
+                    | None -> Some ConstVoid
+                    | Some cv -> Some cv
+                else None
+
+            let env'' = Interpreter.leave_block infos env'' (output::input) env'
+            let red = (env, env'', res)
+            TrExprAction (red, input, output, tr_args, tr)
+        else
+            let red = (env, env', None)
+            TrExprAction (red, input, output, tr_args, TrNotEvaluated)
+
+    and trace_action (m:ModuleDecl) infos (env:Model.Environment) name args =
+        let action_decl = find_action m name
+        let effect env = trace_statement m infos env action_decl.Content
+        trace_inline_action m infos env action_decl.Args action_decl.Output effect args
--- invariant_synthesis/InvariantSynthesis/InvariantSynthesis/Trace.fs
+++ invariant_synthesis/InvariantSynthesis/InvariantSynthesis/Trace.fs
@@ -4,7 +4,7 @@
     open Model
     open System.Drawing
 
-    type RuntimeStData = Environment * Environment
+    type RuntimeStData = Environment * Environment * bool
     type RuntimeExprData = Environment * Environment * ConstValue option
 
     type TrExpression =
@@ -49,6 +49,23 @@
         | TrExprSomeElse (red,_,_,_) -> Some red
         | TrExprNotEvaluated -> None
 
+    let runtime_data_of_st s =
+        match s with
+        | TrNewBlock (red,_,_)
+        | TrExpression (red,_)
+        | TrVarAssign (red,_,_)
+        | TrFunAssign (red,_,_,_)
+        | TrForallFunAssign (red,_,_,_)
+        | TrIfElse (red,_,_)
+        | TrIfSomeElse (red,_,_,_,_)
+        | TrAssert (red,_) -> Some red
+        | TrNotEvaluated -> None
+
+    let st_is_fully_executed s =
+        match runtime_data_of_st s with
+        | None -> false
+        | Some (_,_,b) -> b
+
     let expr_is_fully_evaluated expr =
         match runtime_data_of_expr expr with
         | None -> false
@@ -58,11 +75,19 @@
     let exprs_are_fully_evaluated exprs =
         List.forall expr_is_fully_evaluated exprs
 
+    let sts_are_fully_executed sts =
+        List.forall st_is_fully_executed sts
+
     exception NoRuntimeData
     exception NotFullyEvaluated
 
     let final_env_of_expr expr =
         match runtime_data_of_expr expr with
+        | None -> raise NoRuntimeData
+        | Some (_,env,_) -> env
+
+    let final_env_of_st st =
+        match runtime_data_of_st st with
         | None -> raise NoRuntimeData
         | Some (_,env,_) -> env
 
@@ -72,6 +97,13 @@
                 final_env_of_expr e
             with :? NoRuntimeData -> acc
         List.fold aux initial_env exprs
+
+    let final_env_of_sts sts initial_env =
+        let aux acc s =
+            try
+                final_env_of_st s
+            with :? NoRuntimeData -> acc
+        List.fold aux initial_env sts
 
     let initial_env_of_expr expr =
         match runtime_data_of_expr expr with
