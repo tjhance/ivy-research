b1de288d402fc534d02549b34b3d845a73780291
E-Sh4rk <esh4rk@gmail.com>
2018-06-01 17:20:47 -0400

    update

--- invariant_synthesis/InvariantSynthesis/InvariantSynthesis/TSynthesis.fs
+++ invariant_synthesis/InvariantSynthesis/InvariantSynthesis/TSynthesis.fs
@@ -2,17 +2,19 @@
 
     open AST
     open Trace
-    open TInterpreter
-    open Interpreter
-    open System
 
     let marks_for_value = Synthesis.marks_for_value
     let marks_for_formula = Synthesis.marks_for_formula
     let config_union = Synthesis.config_union
     let is_var_marked = Synthesis.is_var_marked
+    let is_fun_marked = Synthesis.is_fun_marked
+    let remove_var_marks = Synthesis.remove_var_marks
+    let remove_fun_marks = Synthesis.remove_fun_marks
     let config_enter_block = Synthesis.config_enter_block
     let config_leave_block = Synthesis.config_leave_block
     let is_better_config = Synthesis.is_better_config
+    let compute_neighbors_with_perm = Synthesis.compute_neighbors_with_perm
+    let add_ineq_between = Synthesis.add_ineq_between
 
     let rec marks_before_expression module_decl infos tr cfg mark_value =
         match tr with
@@ -70,10 +72,10 @@
             else aux false false
         | TrExprAnd ((env,env',cv), tr_e1, tr_e2) ->
             let (env1,env1',v1) = runtime_data_of_expr tr_e1
-            let tr_not_e1 = TrExprNot ((env1,env1',Option.map value_not v1), tr_e1)
+            let tr_not_e1 = TrExprNot ((env1,env1',Option.map Interpreter.value_not v1), tr_e1)
             let (env2,env2',v2) = runtime_data_of_expr tr_e2
-            let tr_not_e2 = TrExprNot ((env2,env2',Option.map value_not v2), tr_e2)
-            let tr_or = TrExprOr ((env, env', Option.map value_not cv), tr_not_e1, tr_not_e2)
+            let tr_not_e2 = TrExprNot ((env2,env2',Option.map Interpreter.value_not v2), tr_e2)
+            let tr_or = TrExprOr ((env, env', Option.map Interpreter.value_not cv), tr_not_e1, tr_not_e2)
             marks_before_expression module_decl infos (TrExprNot ((env,env',cv), tr_or)) cfg mark_value
         | TrExprNot (_,tr) -> marks_before_expression module_decl infos tr cfg mark_value
         | TrExprSomeElse ((env,_,rv),_,d,f,v) ->
@@ -90,7 +92,58 @@
         List.fold2 aux cfg (List.rev trs) (List.rev mark_values)
 
     and marks_before_statement module_decl infos tr cfg =
-        Synthesis.empty_config // TODO
+        match tr with
+        | TrNotEvaluated _ -> cfg
+        | TrNewBlock (_, decls, tr_sts) ->
+            let cfg' = config_enter_block infos cfg decls
+            let cfg' = marks_before_statements module_decl infos tr_sts cfg'
+            config_leave_block infos cfg' decls cfg
+        | TrExpression (_, tr_e) -> marks_before_expression module_decl infos tr_e cfg false
+        | TrVarAssign (_, str, tr_e) ->
+            let (cfg, marked) =
+                if expr_is_fully_evaluated tr_e then
+                    let marked = is_var_marked infos cfg str
+                    let cfg = remove_var_marks infos cfg str
+                    (cfg, marked)
+                else (cfg, false)
+            marks_before_expression module_decl infos tr_e cfg marked
+        | TrFunAssign (rsd, str, tr_es, tr_e) ->
+            (*
+            fun(ei)=e
+            ei ---eval---> vi
 
-    and marks_before_statements module_decl infos tr cfg =
-        Synthesis.empty_config // TODO
+            Two cases:
+            fun(vi) is marked ->    We mark all ei, we add necessary inequalities, we mark e.
+                                    We remove mark on fun(vi)
+            otherwise ->    We mark all ei s.t. there exists wi different from ei with fun(...wi...) marked, 
+                            we add necessary inequalities
+            *)
+            let tr_all = tr_es@[tr_e]
+            if exprs_are_fully_evaluated tr_all then
+                let vs = List.map ret_value_of_expr tr_es
+                let marked = is_fun_marked infos cfg str vs
+                let cfg = remove_fun_marks infos cfg str vs
+                
+                let n = List.length vs
+                let permutations = Helper.all_permutations n
+                let possibilities = Seq.map (compute_neighbors_with_perm infos cfg marked str vs Helper.identity Helper.identity) permutations
+
+                let cfg = marks_before_expression module_decl infos tr_e cfg marked
+
+                let treat_possibility (marks, neighbors) =
+                    let (m,um,ad) = cfg
+                    let (m,um,ad) = marks_before_expressions module_decl infos tr_es (m,um,ad) marks
+                    let ad = Seq.fold2 (fun ad v ns -> add_ineq_between infos ad (Set.singleton v) ns) ad vs neighbors
+                    (m,um,ad)
+
+                let results = Seq.map treat_possibility possibilities
+                Helper.seq_min is_better_config results
+            else
+                marks_before_expressions module_decl infos tr_all cfg (List.map (fun _ -> false) tr_all) 
+        // TODO
+
+    // Statements are analysed in reverse order
+    and marks_before_statements module_decl infos trs cfg =
+        let aux cfg tr =
+            marks_before_statement module_decl infos tr cfg
+        List.fold aux cfg trs
