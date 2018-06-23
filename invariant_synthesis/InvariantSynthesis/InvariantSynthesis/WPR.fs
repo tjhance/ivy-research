25218c9b4ff5e4f51f9e22edd947dbf0f423dfe1
E-Sh4rk <esh4rk@gmail.com>
2018-06-22 20:17:05 -0400

    WPR

---
+++ invariant_synthesis/InvariantSynthesis/InvariantSynthesis/WPR.fs
@@ -0,0 +1,14 @@
+﻿module WPR
+
+    open MinimalAST
+
+    type Statement =
+        | NewBlock of List<VarDecl> * List<Statement>
+        | VarAssign of string * Value
+        | VarAssignAction of string * string * List<Value>
+        | FunAssign of string * List<VarDecl> * Value
+        | Parallel of Statement * Statement
+        | Assume of Value
+        | Abort
+
+
