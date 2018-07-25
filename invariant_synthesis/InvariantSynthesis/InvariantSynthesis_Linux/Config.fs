5a7eedb6856ece67324e5f5d755b9d9db90382eb
E-Sh4rk <esh4rk@gmail.com>
2018-07-25 12:01:52 -0400

    added linux configuration

---
+++ invariant_synthesis/InvariantSynthesis/InvariantSynthesis_Linux/Config.fs
@@ -0,0 +1,6 @@
+﻿module Config
+
+    let parser_cmd = "parser.native"
+    let parser_args = "all %IN% %OUT% %ERR%"
+    let parser_output_path = "parser.out"
+    let parser_error_path = "parser.err"
