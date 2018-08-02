9ac24a58830f9aa54e4a2d6712ed80b8c047fb48
Travis Hance <tjhance7@gmail.com>
2018-08-02 16:22:21 -0400

    make better Makefile

--- invariant_synthesis/InvariantSynthesis/run.sh
+++ invariant_synthesis/InvariantSynthesis/run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-mono InvariantSynthesis_DotNet4/bin/Debug/InvariantSynthesis_DotNet4.exe $1
+#mono InvariantSynthesis_DotNet4/bin/Debug/InvariantSynthesis_DotNet4.exe $1
+mono bin/InvariantSynthesis.exe $1
