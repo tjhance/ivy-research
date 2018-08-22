ac7b0eab8a7624c4d7e094b955b5ab14e1edc637
Travis Hance <tjhance7@gmail.com>
2018-08-22 12:36:37 -0400

    use Linux config, revert the windows one

--- invariant_synthesis/InvariantSynthesis/InvariantSynthesis_DotNet4/Config.fs
+++ invariant_synthesis/InvariantSynthesis/InvariantSynthesis_DotNet4/Config.fs
@@ -1,6 +1,6 @@
 ﻿module Config
 
-    let parser_cmd = "Parser/main.native"
-    let parser_args = "all %IN% %OUT% %ERR%"
+    let parser_cmd = "lin"
+    let parser_args = "parser.native all %IN% %OUT% %ERR%"
     let parser_output_path = "parser.out"
     let parser_error_path = "parser.err"
