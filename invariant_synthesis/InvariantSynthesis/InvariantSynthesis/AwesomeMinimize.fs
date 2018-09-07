bd7912aee41de105373abee5029320b1472829fa
Travis Hance <tjhance7@gmail.com>
2018-09-06 21:27:06 -0400

    get AwesomeMinimize to work for the elt example

--- invariant_synthesis/InvariantSynthesis/InvariantSynthesis/AwesomeMinimize.fs
+++ invariant_synthesis/InvariantSynthesis/InvariantSynthesis/AwesomeMinimize.fs
@@ -44,6 +44,11 @@ module AwesomeMinimize
           | Z3Forall (de, b) -> List.map (fun x -> Z3Forall (de, x)) (get_conjuncts_including_forall b)
           | _ -> [v]
 
+  let rec get_conjuncts (v: Z3Value) =
+      match v with
+          | Z3And(a, b) -> List.append (get_conjuncts a) (get_conjuncts b)
+          | _ -> [v]
+
   let rec get_disjuncts (v: Z3Value) =
       match v with
           | Z3Or (a, b) -> List.append (get_disjuncts a) (get_disjuncts b)
@@ -67,7 +72,7 @@ module AwesomeMinimize
       | MFun of string * List<MValue>
       | MEqual of MValue * MValue
       | MOr of (MValue list) ref
-      | MAnd of MValue * MValue
+      | MAnd of (MValue list) ref
       | MNot of MValue
       | MIfElse of MValue * MValue * MValue
       | MForall of VarDecl * MValue
@@ -82,7 +87,7 @@ module AwesomeMinimize
               | Z3Equal (a, b) -> MEqual (aux a, aux b)
               | Z3Or _ -> MOr (ref (List.map aux (get_disjuncts v)))
               | Z3Imply _ -> failwith "z3_to_m: Z3Imply not expected"
-              | Z3And (a, b) -> MAnd (aux a, aux b)
+              | Z3And _ -> MAnd (ref (List.map aux (get_conjuncts v)))
               | Z3Not a -> MNot (aux a)
               | Z3IfElse (a,b,c) -> MIfElse (aux a, aux b, aux c)
               | Z3Forall (de, b) -> MForall (de, aux b)
@@ -98,7 +103,7 @@ module AwesomeMinimize
               | MFun (s, vs) -> Z3Fun (s, List.map aux vs)
               | MEqual (a, b) -> Z3Equal (aux a, aux b)
               | MOr l -> or_list (List.map aux !l)
-              | MAnd (a, b) -> Z3And (aux a, aux b)
+              | MAnd l -> and_list (List.map aux !l)
               | MNot a -> Z3Not (aux a)
               | MIfElse (a,b,c) -> Z3IfElse (aux a, aux b, aux c)
               | MForall (de, b) -> Z3Forall (de, aux b)
@@ -124,19 +129,45 @@ module AwesomeMinimize
         | Solver.SolverResult.SAT _ -> printfn "sat\n"; false
       *)
 
+    let cur_invariants = Solver.z3_formula_for_axioms_and_conjectures mmd
+    let is_redundant (a: Z3Value) (b: Z3Value) : bool =
+      let v = Z3Not (Z3Imply (Z3And (cur_invariants, a), b))
+      let z3ctx = Z3Utils.build_context md
+      let z3e = Z3Utils.build_value z3ctx Map.empty Map.empty v
+      match Z3Utils.check z3ctx z3e 5000 with
+        | Z3Utils.UNSAT -> true
+        | Z3Utils.UNKNOWN -> failwith "got unknown"
+        | Z3Utils.SAT model ->
+            (*printfn "%s\n" (model.ToString())*)
+            false
+
     let minimize_part v =
       printfn "trying to minimize %s" (Printer.z3value_to_string v)
       let tree = z3_to_m v
 
-      let rec traverse_and_edit_tree subtree : unit =
+      let rec traverse_and_edit_tree subtree inside_e : unit =
         match subtree with
           | MForall (de, a) ->
-              traverse_and_edit_tree a
+              traverse_and_edit_tree a inside_e
           | MExists (de, a) ->
-              traverse_and_edit_tree a
-          | MAnd (a, b) ->
-              traverse_and_edit_tree a
-              traverse_and_edit_tree b
+              traverse_and_edit_tree a true
+          | MAnd lref ->
+              let children = !lref
+
+              if not inside_e then
+                let chosen = ref []
+                for r in children do
+                  lref := !chosen
+                  let a1 = m_to_z3 tree
+                  lref := [r]
+                  let a2 = m_to_z3 tree
+                  if not (is_redundant a1 a2) then
+                    chosen := List.append (!chosen) [r]
+                lref := !chosen
+                
+              List.iter (fun x -> traverse_and_edit_tree x inside_e) (!lref)
+                
+                    
           | MOr lref ->
               let pieces = ref (!lref)
               let chosen = ref []
@@ -151,14 +182,15 @@ module AwesomeMinimize
                         chosen := List.append (!chosen) [piece]
               lref := !chosen
 
-              List.iter traverse_and_edit_tree (!lref)
+              List.iter (fun x -> traverse_and_edit_tree x inside_e) (!lref)
           | _ ->
               ()
 
-      traverse_and_edit_tree tree
+      traverse_and_edit_tree tree false
       let result = m_to_z3 tree
       printfn "got result %s" (Printer.z3value_to_string result)
       result
 
-    let minimized = and_list (List.map minimize_part (get_conjuncts_including_forall v))
+    //let minimized = and_list (List.map minimize_part (get_conjuncts_including_forall v))
+    let minimized = minimize_part v
     minimized
